d6f0d2c01ffa7c082a6570c6471a2418345e08b8
JB <janbartczak94@gmail.com>
2020-01-16 20:49:58 +0100

    Testy z JSON

--- Program.cs
+++ Program.cs
@@ -1,180 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
-using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace Examples.System.Net.NetworkInformation.PingTest
+namespace PLCsPing
 {
-    public class PingExample
+    class Program
     {
-        // args[0] can be an IPaddress or host name.
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions();
 
-            // Use the default Ttl value which is 128,
-            // but change the fragmentation behavior.
-            options.DontFragment = true;
-
-            // Create a buffer of 32 bytes of data to be transmitted.
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 120;
-
-            PingReply reply = pingSender.Send("192.168.1.1", timeout, buffer, options);
-
-            if (reply.Status == IPStatus.Success)
+            List<string> ListaSterownikow = ListaPLC();
+            double dRespondTime;
+            string PLCIP = "0";
+            for (int i = 0; i < ListaSterownikow.Count; i++)
             {
-                Console.WriteLine("Address: {0}", reply.Address.ToString());
-                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
-                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
-                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
-                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+                PLCIP = ListaSterownikow[i];
+                Console.WriteLine("Pingowany sterownik: {0}", PLCIP);
+                dRespondTime = PingTimeAverage(PLCIP, 2);
+                Console.WriteLine("Odpowiedź z adresu: {0} wynosi {1}ms - {2}", PLCIP, dRespondTime, CommunicationStatus(dRespondTime));
+                System.Threading.Thread.Sleep(1000);
             }
 
-            Odległość p = new Odległość();
-            p.WartośćMetr = 12;
-            Console.WriteLine("Buffer size: {0}", p.WartośćMila);
-
-            int? liczba = null;
- 
-            Console.WriteLine("[1] {0}", liczba ?? 3);
-            liczba += 12;
-            Console.WriteLine("[2] {0}", liczba ?? 1);
-            liczba = 1234;
-            Console.WriteLine("[3] {0}", liczba);
-            liczba += 12;
-            Console.WriteLine("[4] {0}", liczba);
-
-            int liczba32 = 3;
-            long liczba64 = 3L;
-
-            Console.WriteLine("{0} {1}", liczba32, liczba32.GetType());
-            Console.WriteLine("{0} {1}", liczba64, liczba64.GetType());
-
-            static int iloczyn(int a, int b, int c = 1)
-            {
-                return a * b * c;
-            }
-
-            float? wynik; 
-
-            wynik = iloczyn(b: 7, a: 8);
-            Console.WriteLine("{0}", wynik);
-
-            var zmienna1 = new { A = 1, B = 2 };
-            Console.WriteLine(zmienna1);
-
-
-            int[] dane = { 1, 5, 3, 2, 4 };
-            int max = dane[0], min = dane[0];
-            double średnia = dane[0];
-            for (int i = 1; i < dane.Length; i++)
-            {
-                if (dane[i] > max)
-                {
-                    max = dane[i];
-                }
-                else if (dane[i] < min)
-                {
-                    min = dane[i];
-                }
-                średnia += dane[i];
-            }
-            średnia /= dane.Length;
-            var podsumowanie = new { max, min, średnia };
-            Console.WriteLine(podsumowanie);
-
-/*
-            string tekst = "";
-            for (int i = 0; i < 10000; i++)
-            {
-                tekst += "Uczę się jak szybko tworzyć teksty w języku C#\n";
-            }
-*/
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 10000; i++)
-            {
-                builder.Append("Uczę się jak szybko tworzyć teksty w języku C#\n");
-            }
-
-
-            StringBuilder tekst = new StringBuilder();
-            tekst.Append("Pierwsza linijka\n");
-            tekst.AppendLine("Druga linijka");
-            tekst.Append('*', 10);
-            Console.WriteLine(tekst.ToString());
-
-
-
-            tekst.Remove(0, tekst.Length);
-            tekst.Append("Nowy do wyświetlenia");
-            tekst.Insert(5, "tekst ");
-            Console.WriteLine(tekst.ToString());
-
-
-            static int[,] GenerujTabliczke(int n)
-            {
-                int[,] tab = new int[n, n];
-                for (int x = 0; x < n; x++)
-                {
-                    for (int y = 0; y < n; y++)
-                    {
-                        tab[x, y] = x * y;
-                    }
-                }
-                return tab;
-            }
-
-
-            Stopwatch stopWatch = new Stopwatch();
-            TimeSpan min2 = new TimeSpan(long.MaxValue);
-            TimeSpan max2 = new TimeSpan(0);
-            Console.WriteLine("Min.\t\t\tTeraz\t\t\tMax.");
-            Console.Write("Rozpocznij wciskanie guzików...\n");
+            Console.WriteLine("Koniec pingowania: {0}", PLCIP);
             Console.ReadKey();
-
-            ConsoleKeyInfo key;
-            while (true)
-            {
-                stopWatch.Reset();
-                stopWatch.Start();
-                key = Console.ReadKey();
-                stopWatch.Stop();
-                if (min2 > stopWatch.Elapsed)
-                {
-                    min2 = stopWatch.Elapsed;
-                }
-                else if (stopWatch.Elapsed > max2)
-                {
-                    max2 = stopWatch.Elapsed;
-                }
-                if(key.Key == ConsoleKey.Enter)
-                {
-                    break;
-                }
-                Console.Write("\r{0}\t{1}\t{2} ", min, stopWatch.Elapsed, max);
-            }
-
-
         }
-        class Odległość
+
+        public static double PingTimeAverage(string host, int echoNum)
         {
-            private const double mn_mila = 8554.3;
-            private double odl = 0;
-            public double WartośćMetr
+            long totalTime = 0;
+            int timeout = 120;
+            Ping pingSender = new Ping();
+
+            for (int i = 0; i < echoNum; i++)
             {
-                get { return odl; }
-                set { odl = value; }
+                PingReply reply = pingSender.Send(host, timeout);
+                if (reply.Status == IPStatus.Success)
+                {
+                    totalTime += reply.RoundtripTime;
+                }
+                if (reply.Status == IPStatus.TimedOut)
+                {
+                    totalTime = 9999 * echoNum;
+                }
             }
-            public double WartośćMila
-            {
-                get { return odl / mn_mila; }
-                set { odl = mn_mila * value; }
-            }
+            return totalTime / echoNum;
         }
+
+        public static bool CommunicationStatus(double time)
+        {
+            bool xStatusOfCommunication;
+            if (time < 200)
+            {
+                xStatusOfCommunication = true;
+            }
+            else
+            {
+                xStatusOfCommunication = false;
+            }
+            return xStatusOfCommunication;
+        }
+        public static List<string> ListaPLC()
+        {
+            List<string> ListaPLC = new List<string>();
+            ListaPLC.Add("192.168.100.80");
+            ListaPLC.Add("192.168.100.81");
+            ListaPLC.Add("192.168.100.82");
+            ListaPLC.Add("192.168.100.83");
+            return ListaPLC;
+        }
+
+        public partial class PlcList
+        {
+            [JsonProperty("Adres1")]
+            public string Adres1 { get; set; }
+
+            [JsonProperty("Adres2")]
+            public string Adres2 { get; set; }
+        }
+
     }
 }
-
