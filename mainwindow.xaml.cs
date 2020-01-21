95b8da32ecdc15153021187ca4ada711da405fe2
JB <janbartczak94@gmail.com>
2020-01-21 19:08:48 +0100

    Async + New list of view

--- mainwindow.xaml.cs
+++ mainwindow.xaml.cs
@@ -13,62 +13,128 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
 
 namespace PLCsPing
 {
     public partial class MainWindow : Window
     {
-
+        private List<DataToPlot> DataToSend = null;
         public MainWindow()
         {
 
             InitializeComponent();
+            Clock();
+            asyncWorking();
 
-            List<string> slPLCList = PLCsList();
-            string sPLCIP;
-            double dRespondTime = 0;
-            int iThreadSleep = 50;
-            int iLoopCount = 1;
-            bool xCommunicationStatus = false;
-
-            foreach (string item in slPLCList)
-            {
-                DataToPlot DataToPlot = new DataToPlot();
-                sPLCIP = item;
-                DataToPlot.AddresIP = sPLCIP;
-                for (int i = 0; i < iLoopCount; i++)
-                {
-                    dRespondTime = PingTimeAverage(sPLCIP, 2);
-                    DataToPlot.ResponseTime = dRespondTime;
-                    xCommunicationStatus = CommunicationStatus(dRespondTime);
-                    DataToPlot.CommunicationStatus = xCommunicationStatus;
-                    System.Threading.Thread.Sleep(iThreadSleep);
-                }
-                PLCsDataGrid.Items.Add(DataToPlot);
-            }
+            InitBinding();
 
         }
+        private void InitBinding()
+        {
+/*
+            DataToSend = new List<Person>();
+            DataToSend.Add(new Person(1, "Jan", "Kowalski", 25));
+            DataToSend.Add(new Person(2, "Adam", "Nowak", 24));
+            DataToSend.Add(new Person(3, "Agnieszka", "Kowalczyk", 23));
+
+            lstPersons.ItemsSource = m_oPersonList;*/
+        }
+
 
         public static List<string> PLCsList()
         {
             List<string> Address = new List<string>();
-            Address.Add("192.168.100.80");
+            Address.Add("192.168.1.1");
             Address.Add("192.168.100.81");
             Address.Add("192.168.100.82");
             Address.Add("192.168.100.83");
             return (Address);
         }
+        private async void asyncWorking()
+        {
+            DataToSend = new List<DataToPlot>();
+            bool xFirstLoop = false;
+            while (true)
+            {
 
-        public class DataToPlot
+                List<string> slPLCList = PLCsList();
+                string sPLCIP;
+                double dRespondTime = 0;
+                int iThreadSleep = 50;
+                int iLoopCount = 1;
+                bool xCommunicationStatus = false;
+               
+                foreach (string item in slPLCList)
+                {
+                    sPLCIP = item;
+                    for (int i = 0; i < iLoopCount; i++)
+                    {
+                        await Task.Run(() =>
+                        {
+                            dRespondTime = PingTimeAverage(sPLCIP, 2);
+                            xCommunicationStatus = CommunicationStatus(dRespondTime);
+                            System.Threading.Thread.Sleep(iThreadSleep);
+                            if (xFirstLoop == false)
+                            {
+                                DataToSend.Add(new DataToPlot(sPLCIP, dRespondTime, xCommunicationStatus));
+                            }
+                        });
+                    }
+
+                }
+                if(xFirstLoop == false)
+                {
+                    lstPersons.ItemsSource = DataToSend;
+                    xFirstLoop = true;
+                }
+                // <!-- TERAZ TU ZROBIC ZEBY AKTUALIZOWAC LISTE --!>
+            }
+        }
+
+        private void Clock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        public class Person
+        {
+            public int PersonId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+
+            public Person(int nPersonId, string sFirstName, string sLastName,
+                int nAge)
+            {
+                PersonId = nPersonId;
+                FirstName = sFirstName;
+                LastName = sLastName;
+                Age = nAge;
+            }
+        }
+        private class DataToPlot
         {
             public string AddresIP { get; set; }
             public double ResponseTime { get; set; }
             public bool CommunicationStatus { get; set; }
+
+            public DataToPlot(string nAddresIP, double nResponseTime, bool nCommunicationStatus)
+            {
+                AddresIP = nAddresIP;
+                ResponseTime = nResponseTime;
+                CommunicationStatus = nCommunicationStatus;
+            }
         }
 
+        void timer_Tick(object sender, EventArgs e)
+        {
+            ClockTime.Content = DateTime.Now.ToLongTimeString();
+        }
 
-        public static double PingTimeAverage(string host, int echoNum)
+        private static double PingTimeAverage(string host, int echoNum)
         {
             long totalTime = 0;
             int timeout = 120;
@@ -89,7 +155,7 @@ namespace PLCsPing
             return totalTime / echoNum;
         }
 
-        public static bool CommunicationStatus(double time)
+        private static bool CommunicationStatus(double time)
         {
             bool xStatusOfCommunication;
             if (time < 200)
@@ -102,7 +168,6 @@ namespace PLCsPing
             }
             return xStatusOfCommunication;
         }
-
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
